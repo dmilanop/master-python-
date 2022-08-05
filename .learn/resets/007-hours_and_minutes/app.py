c4a0e5a712e1ca5eafdbc40832ec34dca9794984
dmilanop <dmilanop28@gmail.com>
2022-08-05 01:04:57 +0000

    Listo por hoy

--- .learn/resets/007-hours_and_minutes/app.py
+++ .learn/resets/007-hours_and_minutes/app.py
@@ -1,9 +1,7 @@
 #Complete the function to calculate how many hours and minutes are passed since midnight.
 def hours_minutes(secs):
-  secs_hours = int(secs / 3600)
-  secs_mint = int(secs / 60)
-
-  return secs_hours, secs_mint
+  
+  return None
 
 #Invoke the funtion and pass any interger as its argument.
 print(hours_minutes(3900))
